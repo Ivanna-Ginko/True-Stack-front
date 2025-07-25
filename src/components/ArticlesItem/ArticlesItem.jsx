cf47606646b66933e0dcb0ca96e3d4554eb1e21d
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-25 23:13:03 +0300

    add styles on comonent

--- src/components/ArticlesItem/ArticlesItem.jsx
+++ src/components/ArticlesItem/ArticlesItem.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+// import ButtonAddToBookmarks from '../ButtonAddToBookmarks/ButtonAddToBookmarks'
+import s from '../ArticlesItem/ArticlesItem.module.css'
 
-const ArticlesItem = () => {
-  return (
-    <div>ArticlesItem</div>
+const ArticlesItem = ({id, image, author, title, description}) => {
+    return (
+    <li className= {s.card}>
+      <img src={image} alt={title} className={s.image}/>
+      <div className={s.content}>
+        <p className={s.author}>
+          {author}
+        </p>
+        <h3 className={s.title}>
+          {title}
+        </h3>
+        <p className={s.description}>
+          {description}
+        </p>
+      </div>
+      <div className= {s.bottom}>
+      <Link to={`/articles/${id}`}>
+        Learn more
+      </Link>
+      {/* <ButtonAddToBookmarks articleId={id}/> */}
+      </div>
+    </li>
   )
 }
 
