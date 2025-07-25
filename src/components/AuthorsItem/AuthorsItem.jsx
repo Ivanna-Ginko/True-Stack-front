0cd17c1f1d447c52d3f2eefef63241654a225371
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-25 22:38:00 +0300

    add ArticlesItem component

--- src/components/AuthorsItem/AuthorsItem.jsx
+++ src/components/AuthorsItem/AuthorsItem.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+// import ButtonAddToBookmarks from '../ButtonAddToBookmarks/ButtonAddToBookmarks'
+import s from '../AuthorsItem/ArticlesItem.module.css'
 
-const AuthorsItem = () => {
+const AuthorsItem = ({id, image, author, title, description}) => {
   return (
-    <div>AuthorsItem</div>
+    <li className= {s.card}>
+      <img src={image} alt={title} className={s.image}/>
+      <div className={s.content}>
+        <p className={s.author}>
+          {author}
+        </p>
+        <h3 className={s.title}>
+          {title}
+        </h3>
+        <p className={s.description}>
+          {description}
+        </p>
+      </div>
+      <div className= {s.bottom}>
+      <Link to={`/articles/${id}`}>
+        Learn more
+      </Link>
+      </div>
+      {/* <ButtonAddToBookmarks articleId={id}/> */}
+    </li>
   )
 }
 
