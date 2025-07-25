a60ff580d17a9ea840ebbf759ca53e968a08c874
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-25 23:14:00 +0300

    fixed articleList

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -6,7 +6,6 @@ const ArticlesList = () => {
   return (
     <>
         <h1>Articlelist</h1>
-        <ArticlesItem id = '1' image='https://unsplash.com/photos/programming-code-abstract-technology-background-of-software-developer-and-computer-script-ltpb_WinC3Y' author='Clark' title='When Anxiety Feels Like a Room With No Doors' description='A deeply personal reflection on living with generalized anxiety and the small rituals that help reclaim control, one breath at a time.'/>
         <LoadMore />
     </>
   )
