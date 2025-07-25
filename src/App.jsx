55fa62f8d041bcabf24d839c1c059801a6917767
Illia Liashko <2275040@gmail.com>
2025-07-25 20:31:21 +0300

    create redux without api paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,67 @@
-import './App.css'
-import { Routes, Route } from "react-router-dom";
-import Footer from './components/Footer/Footer.jsx'
-import Outlet from './components/Outlet/Outlet.jsx'
-import Header from './components/Header/Header.jsx'
-import RegisterPage from './Pages/RegisterPage/RegisterPage.jsx'
-import LoginPage from './Pages/LoginPage/LoginPage.jsx'
-import ArticlesPage from './Pages/ArticlesPage/ArticlesPage.jsx'
-import ArticlePage from './Pages/ArticlePage/ArticlePage.jsx'
-import AuthorsPage from './Pages/AuthorsPage/AuthorsPage.jsx'
-import AuthorProfilePage from './Pages/AuthorProfilePage/AuthorProfilePage.jsx'
-import CreateArticlePage from './Pages/AuthorProfilePage/AuthorProfilePage.jsx'
-
-
-
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer.jsx';
+import Outlet from './components/Outlet/Outlet.jsx';
+import Header from './components/Header/Header.jsx';
+import RegisterPage from './Pages/RegisterPage/RegisterPage.jsx';
+import LoginPage from './Pages/LoginPage/LoginPage.jsx';
+import ArticlesPage from './Pages/ArticlesPage/ArticlesPage.jsx';
+import ArticlePage from './Pages/ArticlePage/ArticlePage.jsx';
+import AuthorsPage from './Pages/AuthorsPage/AuthorsPage.jsx';
+import AuthorProfilePage from './Pages/AuthorProfilePage/AuthorProfilePage.jsx';
+import CreateArticlePage from './Pages/AuthorProfilePage/AuthorProfilePage.jsx';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { authenticateUser } from './redux/operations.js';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authenticateUser());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-
-     <Header />
-     <Routes>
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<Outlet />} />
-      <Route path="/articles" element={<ArticlesPage />} />
-      <Route path="/articles/:id" element={<ArticlePage />} />
-      <Route path="/authors" element={<AuthorsPage />} />
-      <Route path="/authors/:id" element={<AuthorProfilePage />} />
-      <Route path="/create" element={<CreateArticlePage />} />
-     </Routes>
-     <Footer/>
+      <Header />
+      <Routes>
+        <Route
+          path='/register'
+          element={<RegisterPage />}
+        />
+        <Route
+          path='/login'
+          element={<LoginPage />}
+        />
+        <Route
+          path='/'
+          element={<Outlet />}
+        />
+        <Route
+          path='/articles'
+          element={<ArticlesPage />}
+        />
+        <Route
+          path='/articles/:id'
+          element={<ArticlePage />}
+        />
+        <Route
+          path='/authors'
+          element={<AuthorsPage />}
+        />
+        <Route
+          path='/authors/:id'
+          element={<AuthorProfilePage />}
+        />
+        <Route
+          path='/create'
+          element={<CreateArticlePage />}
+        />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate
+        loading={<p>Loading...</p>}
+        persistor={persistor}
+      >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
