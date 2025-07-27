f179b21b112ee7bffba5ee5cd0bc0256ee9ddc64
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-27 15:45:48 +0300

    add new component

---
+++ src/components/PopularArticles/PopularArticles.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AppLink from '../AppLink/AppLink'
+
+const PopularArticles = () => {
+    return (
+        <>
+        <div>
+            <h2></h2>
+            <AppLink/>
+        </div>
+        <ul></ul>
+        </>
+    )
+}
+
+export default PopularArticles
