629f1d7ef0a6970aee54a579ce9b29362d705983
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-27 21:29:20 +0300

    add styles

--- src/components/PopularArticles/PopularArticles.jsx
+++ src/components/PopularArticles/PopularArticles.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppLink from '../AppLink/AppLink'
+import axios from 'axios';
+import ArticlesItem from '../ArticlesItem/ArticlesItem'
+import s from './PopularArticles.module.css'
+import Container from '../container/Container'
 
 const PopularArticles = () => {
+    const [articles, setArticles] = useState([]);
+    useEffect(()=>{
+        const fetchPopularArticles = async () =>{
+            try{
+                const res = await axios.get();
+                setArticles(res.data);
+            }
+            catch(error){
+                console.error('❌ Помилка при завантаженні статей:', error.message);
+            }
+        };
+        fetchPopularArticles();
+    },[]);
     return (
         <>
-            <div>
-                <h2></h2>
-                <AppLink>
-                    Go to all Articles
-                    <svg>
-                        <use href=''></use>
-                    </svg>
-                </AppLink>
-            </div>
-            <ul></ul>
+            <Container>
+                <div className= {s.titleContainer}>
+                    <h2 className={s.title}>Popular Articles</h2>
+                    <AppLink variant='link' size='lg' to='/articles'> 
+                        Go to all Articles
+                        <svg width={14.25} height={14.25}>
+                            <use href='/src/assets/icons/arrow.svg#icon-arrow'></use>
+                        </svg>
+                    </AppLink>
+                </div>
+                <ul>
+                    {articles.map(article => (
+                        <ArticlesItem ket={article.id} id={article.id} image={article.image} author={article.author} title={article.title} description={article.description}/>
+                    ))}
+                </ul>
+            </Container>
         </>
     )
 }
