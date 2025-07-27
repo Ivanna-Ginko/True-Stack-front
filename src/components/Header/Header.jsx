bd8bd0238f7b89d1d2844b46a2e174f62f913c11
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-07-27 15:10:58 +0300

    clearTab

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,12 @@
-// import React from "react";
-
-// const Header = () => {
-//   return (
-//     <>
-//       <h1>Header</h1>
-//     </>
-//   );
-// };
-
-// export default Header;
-// для виклика кнопки
-import React, { useState } from "react";
-import ModalErrorSave from "../ModalErrorSave/ModalErrorSave";
+import React from "react";
 
 const Header = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
   return (
     <>
       <h1>Header</h1>
-      <button onClick={openModal}>Викликати модалку</button>
-
-      {isModalOpen && <ModalErrorSave onClose={closeModal} />}
     </>
   );
 };
 
 export default Header;
+
