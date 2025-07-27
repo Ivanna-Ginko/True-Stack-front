4de198aad2590f0e867f2b59fa780f78c8289480
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-27 16:23:58 +0300

    add more tags

--- src/components/PopularArticles/PopularArticles.jsx
+++ src/components/PopularArticles/PopularArticles.jsx
@@ -4,11 +4,16 @@ import AppLink from '../AppLink/AppLink'
 const PopularArticles = () => {
     return (
         <>
-        <div>
-            <h2></h2>
-            <AppLink/>
-        </div>
-        <ul></ul>
+            <div>
+                <h2></h2>
+                <AppLink>
+                    Go to all Articles
+                    <svg>
+                        <use href=''></use>
+                    </svg>
+                </AppLink>
+            </div>
+            <ul></ul>
         </>
     )
 }
