ffd045b8844580b867ba268fa953ab5266ded97d
unknown <ivannabelichenko@gmail.com>
2025-07-29 13:22:41 +0300

    Paggination-start

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Pagination = () => {
+  return (
+    <div>Pagination</div>
+  )
+}
+
+
+
+//https://www.npmjs.com/package/react-paginate - ця бібліотека рекомендована до використання, 
+// але написано це в ТЗ в блоці додаткових завдань. 
+// Я думаю, що якщо бібліотека спрощує життя, використати її ми можемо й не доходячи до додатковий. 
+// Як Вам зручно, загалом)
+
+export default Pagination
