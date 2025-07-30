d5190312b9fb1e40d4a7a3dd5927743736c2a712
unknown <ivannabelichenko@gmail.com>
2025-07-30 19:33:17 +0300

    44

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ArticlesItem from '../ArticlesItem/ArticlesItem'
 import LoadMore from '../LoadMore/LoadMore'
 import css from './ArticlesList.module.css'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../../redux/selectors'
+import { fetchArticles } from '../../services/api.js'
 
 //уточнити, помилка через імпорт селектора редакс
 
-const ArticlesList = ({ list, hideFourthOnDesktop = false }) => {
+const ArticlesList = ({ config }) => {
+  const [articleList, setArticleList] = useState ([]);
   const user = useSelector(selectUser)
-  console.log(user)
+  
+  useEffect(() => {
+  const getArticles = async () => {
+    try {
+      const articles = await fetchArticles(config);
+      setArticleList(articles.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  getArticles();
+}, [config]);
+
+  const articlesArr = articleList.data
 
   return (
     <>
-        <ul className={`${css.list} ${hideFourthOnDesktop ? css.hideFourth : ''}`}>
-          {!list && <p>wait....</p>}
-          {list && 
-            list.map(article => {
+        <ul className={`${css.list} }`}>
+          {!articlesArr && <p>wait....</p>}
+          {articlesArr && 
+            articlesArr.map(article => {
               const isAuthor = article.author === user?.user?.id;
               const isSaved = isAuthor && user.savedArticles?.includes(article.id);
                 return (
