2542ad15825c3ca8c7dff234a15ee6669c6b9914
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-07-30 14:49:47 +0300

    addMediaCss

--- src/components/ModalErrorSave/ModalErrorSave.jsx
+++ src/components/ModalErrorSave/ModalErrorSave.jsx
@@ -80,25 +80,3 @@ const ModalErrorSave = ({ onClose }) => {
 };
 
 export default ModalErrorSave;
-
-// для виклика кнопки
-// import React, { useState } from "react";
-// import ModalErrorSave from "../ModalErrorSave/ModalErrorSave";
-
-// const Header = () => {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   const openModal = () => setIsModalOpen(true);
-//   const closeModal = () => setIsModalOpen(false);
-
-//   return (
-//     <>
-//       <h1>Header</h1>
-//       <button onClick={openModal}>Викликати модалку</button>
-
-//       {isModalOpen && <ModalErrorSave onClose={closeModal} />}
-//     </>
-//   );
-// };
-
-// export default Header;
