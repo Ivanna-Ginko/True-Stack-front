1a0756ada1d9a6aef86d24bf95f481f151c3759f
Mykola <ipadikys@gmail.com>
2025-07-30 21:01:10 +0200

    adde hero auth change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import Container from '../container/Container'
 import s from "./Hero.module.css"
 import { useNavigate } from "react-router-dom";
+import { selectIsLoggedIn } from '../../redux/selectors';
 
 const Hero = () => {
   const navigate = useNavigate();
+  const IsLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <>
       <Container>
@@ -13,12 +17,15 @@ const Hero = () => {
               <h1 className={s.hero_header}>Find your <span className={s.hero_span}>harmony</span> in community</h1>
               <div className={s.hero_buttons_container}>
                 <a href="#popularArticles" className={s.hero_articles_btn}>Go to Articles</a>
-                <button
+                {!IsLoggedIn ? <button
                   className={s.hero_reg_btn}
                   onClick={() => navigate("/register")}
                 >
                   Register
-                </button>
+                </button> :
+                  <a href="#topCreators" className={s.hero_reg_btn}>Go to Creators</a>
+                }
+
               </div>
             </div>
           </div>
