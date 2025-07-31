f56b78bf85b3e706c8442aeaa5d3158f064a4487
stillistica <rozhkovavika24@gmail.com>
2025-07-31 18:28:18 +0300

    start AuthorProfilePage

--- src/Pages/AuthorProfilePage/AuthorProfilePage.jsx
+++ src/Pages/AuthorProfilePage/AuthorProfilePage.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
-import LoadMore from '../../components/LoadMore/LoadMore'
-import ArticlesList from '../../components/ArticlesList/ArticlesList'
+import React, { useEffect, useState } from "react";
+// import s from './AuthorProfilePage.module.css';
+import LoadMore from "../../components/LoadMore/LoadMore";
+import ArticlesList from "../../components/ArticlesList/ArticlesList";
+import Container from "../../components/container/Container";
+import { useSelector } from "react-redux";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import { useParams } from "react-router-dom";
+import { fetchAuthorById } from "../../services/api";
+import { selectIsLoggedIn, selectUser } from "../../redux/selectors";
 
 const AuthorProfilePage = () => {
+  const title = "My Profile";
+  const { id: userId } = useParams();
+
+  const user = useSelector(selectUser);
+  const isAuthenticated = useSelector(selectIsLoggedIn);
+  const loggedInUserId = user.id;
+
+  const isMyPage = isAuthenticated && loggedInUserId === userId;
+
+  const [authorData, setAuthorData] = useState(null);
+  const [articlesAmount, setArticlesAmount] = useState(0);
+
+  useEffect(() => {
+    const getAuthorData = async () => {
+      try {
+        const res = await fetchAuthorById(userId);
+        setAuthorData(res.data);
+        setArticlesAmount(0);
+      } catch (error) {
+        console.error("Помилка при отриманні автора", error);
+      }
+    };
+    getAuthorData();
+  }, [userId]);
+
   return (
     <div>
-        <ArticlesList/>
-        <LoadMore/>
+      <Container>
+        {isMyPage ? <SectionTitle title={title} /> : null}
+        {authorData && (
+          <div>
+            <img
+              src={authorData.avatarUrl}
+              alr={`Фото автора ${authorData.name}`}
+            />
+            <div>
+              <h2>{authorData.name}</h2>
+              <p>{articlesAmount} articles</p>
+            </div>
+          </div>
+        )}
+        <ArticlesList />
+        <LoadMore />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default AuthorProfilePage
+export default AuthorProfilePage;
