3a5fafa59350dd99fbc678f33079c253e7adc815
Illia Liashko <2275040@gmail.com>
2025-08-01 21:55:18 +0300

    add layout component

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import css from './Layout.module.css';
+
+export default function Layout({ children }) {
+  return (
+    <div className={css.container}>
+      <Header />
+
+      <main className={css.main}>{children}</main>
+
+      <Footer />
+    </div>
+  );
+}
