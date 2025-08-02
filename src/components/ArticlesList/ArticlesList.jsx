e859f663fa2c12a04843b7a6d2490ef40a5586e9
Nelli <nelli.sedinkina.v@gmail.com>
2025-08-02 20:02:47 +0200

    b4 pull

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,69 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import ArticlesItem from '../ArticlesItem/ArticlesItem'
-import LoadMore from '../LoadMore/LoadMore'
-import css from './ArticlesList.module.css'
-import { useSelector } from 'react-redux'
-import { selectUser } from '../../redux/selectors'
-import { fetchArticles } from '../../services/api.js'
-import toast, { Toaster } from 'react-hot-toast';
+import React, { useState, useEffect } from "react";
+import ArticlesItem from "../ArticlesItem/ArticlesItem";
+import LoadMore from "../LoadMore/LoadMore";
+import css from "./ArticlesList.module.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../redux/selectors";
+import { fetchArticles } from "../../services/api.js";
+import toast, { Toaster } from "react-hot-toast";
 
 //уточнити, помилка через імпорт селектора редакс
 
 const ArticlesList = ({ config, onTotalItemsChange, hideFourthOnDesktop }) => {
-  const [articleList, setArticleList] = useState ([]);
+  const [articleList, setArticleList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState (false)
+  const [isError, setIsError] = useState(false);
 
-  const user = useSelector(selectUser)
-  
+  const user = useSelector(selectUser);
+
   useEffect(() => {
-  const getArticles = async () => {
-    try {
-      setIsLoading(true);
-      const articles = await fetchArticles(config);
-      setArticleList(articles.data.data);
-      console.log(articles.data.data)
-      if (onTotalItemsChange) {
-        onTotalItemsChange(articles.data.data.totalItems);}
-    } catch (error) {
-      setIsError(true);
-      toast.error('Please, try again')
-    } finally {
-      setIsLoading(false);
+    const getArticles = async () => {
+      try {
+        setIsLoading(true);
+        const articles = await fetchArticles(config);
+        setArticleList(articles.data.data);
+        console.log(articles.data.data);
+        if (onTotalItemsChange) {
+          onTotalItemsChange(articles.data.data.totalItems);
+        }
+      } catch (error) {
+        setIsError(true);
+        toast.error("Please, try again");
+      } finally {
+        setIsLoading(false);
       }
-    }
-  getArticles();
-}, [config]);
+    };
+    getArticles();
+  }, [config]);
 
-  const articlesArr = articleList.data
-  
+  const articlesArr = articleList.data;
 
   return (
     <>
-        <ul className={`${css.list} ${hideFourthOnDesktop ? css.hideFourth : ''} }`}>
-          {!articlesArr && <p>wait....</p>}
-          {articlesArr && 
-            articlesArr.map(article => {
-              const isAuthor = article.author === user?.user?.id;
-              const isSaved = isAuthor && user.savedArticles?.includes(article.id);
-                return (
-                <ArticlesItem
-                  key={article.id}
-                  id={article.id}
-                  title={article.title}
-                  author={article.author}
-                  description={article.title}
-                  image={article.img}
-                  isAuthor = {isAuthor}
-                  saved = {isSaved}/>
-            )}
-          )}
-        </ul>
+      <ul
+        className={`${css.list} ${hideFourthOnDesktop ? css.hideFourth : ""} }`}
+      >
+        {!articlesArr && <p>wait....</p>}
+        {articlesArr &&
+          articlesArr.map((article) => {
+            const isAuthor = article.author === user?.user?.id;
+            const isSaved =
+              isAuthor && user.savedArticles?.includes(article.id);
+            return (
+              <ArticlesItem
+                key={article.id}
+                id={article.id}
+                title={article.title}
+                author={article.author}
+                description={article.title}
+                image={article.img}
+                isAuthor={isAuthor}
+                saved={isSaved}
+              />
+            );
+          })}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default ArticlesList
-
-
-
+export default ArticlesList;
