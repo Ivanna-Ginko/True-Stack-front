11ee36ce0109a2379e8629b5cfb019b4f4d387b3
Sofia Besedina <besedina.sofiia@gmail.com>
2025-08-03 20:16:53 +0300

    test

--- src/components/EditArticleButton/EditArticleButton.jsx
+++ src/components/EditArticleButton/EditArticleButton.jsx
@@ -45,8 +45,43 @@
 
 // export default EditArticleButton;
 
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import styles from './EditArticleButton.module.css';
+
+// const EditArticleButton = ({ articleId, title, text, photo }) => {
+//   const navigate = useNavigate();
+//   const [isEdit, setIsEdit] = useState(false);
+//   const [loading] = useState(false);
+
+//   const handleEditClick = () => {
+//     setIsEdit(true);
+//     navigate(`/articles/${articleId}`, {
+//       state: { title, text, photo }
+//     });
+//   };
+
+//   return (
+//     <button
+//       onClick={handleEditClick}
+//       className={`${styles.button} ${isEdit ? styles.edit : ''}`}
+//       disabled={loading}
+//     >
+//       <svg className={`${styles.icon} ${isEdit ? styles.editIcon : ''}`} fill="none">
+//         <path
+//           d="M9.423 30.25h-7.673v-7.688l15.894-15.925M9.423 30.25h20.827M9.423 30.25l15.894-15.925M17.644 6.637l3.288-3.295c2.119-2.123 5.554-2.123 7.673 0s2.119 5.565 0 7.688l-3.288 3.295M17.644 6.637l7.673 7.688"
+//           stroke="currentColor"
+//         />
+//       </svg>
+//     </button>
+//   );
+// };
+
+// export default EditArticleButton;
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import EditIcon from '../../assets/icons/EditArticleButton.svg'; // Імпорт SVG
 import styles from './EditArticleButton.module.css';
 
 const EditArticleButton = ({ articleId, title, text, photo }) => {
@@ -57,7 +92,7 @@ const EditArticleButton = ({ articleId, title, text, photo }) => {
   const handleEditClick = () => {
     setIsEdit(true);
     navigate(`/articles/${articleId}`, {
-      state: { title, text, photo }
+      state: { title, text, photo },
     });
   };
 
@@ -67,12 +102,12 @@ const EditArticleButton = ({ articleId, title, text, photo }) => {
       className={`${styles.button} ${isEdit ? styles.edit : ''}`}
       disabled={loading}
     >
-      <svg className={`${styles.icon} ${isEdit ? styles.editIcon : ''}`} fill="none">
-        <path
-          d="M9.423 30.25h-7.673v-7.688l15.894-15.925M9.423 30.25h20.827M9.423 30.25l15.894-15.925M17.644 6.637l3.288-3.295c2.119-2.123 5.554-2.123 7.673 0s2.119 5.565 0 7.688l-3.288 3.295M17.644 6.637l7.673 7.688"
-          stroke="currentColor"
-        />
-      </svg>
+      <img
+        src={EditIcon}
+        alt="Edit Icon"
+        className={`${styles.icon} ${isEdit ? styles.editIcon : ''}`}
+         aria-label="Edit article"
+      />
     </button>
   );
 };
