1c51f147c3fe6ae481d291ee892dba149a964b9b
SergioUAX <itserver.ua@gmail.com>
2025-08-04 09:57:03 +0200

    temp4

--- src/Pages/AuthorsPage/AuthorsPage.jsx
+++ src/Pages/AuthorsPage/AuthorsPage.jsx
@@ -15,58 +15,60 @@ const AuthorsPage = () => {
   const [isError, setIsError] = useState(false);
 
   const navigate = useNavigate();
+  const perPage = 20;
+
+  const loadAuthors = async (page) => {
+    setIsLoading(true);
+
+    try {
+      const data = await fetchAuthors(page, perPage);
+      const normalizedAuthors = data.data.map((item) => ({
+        ...item,
+        _id: item._id?.$oid || item._id,
+      }));
+
+      if (page === 1) {
+        setAuthors(normalizedAuthors);
+      } else {
+        setAuthors(prev => [...prev, ...normalizedAuthors]);
+      }
+      return normalizedAuthors;
+    } catch (error) {
+      setIsError(true);
+      toast.warning('No authors found', {
+        style: {
+          backgroundColor: 'rgba(209, 224, 216, 1)',
+          color: '#333',
+        },
+      });
+      return [];
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const getAuthors = async () => {
-      try {
-        setIsLoading(true);
-        const data = await fetchAuthors();
-        console.log('API response:', data);
-        const normalizedAuthors = data.data.map((item) => ({
-          ...item,
-          _id: item._id?.$oid || item._id,
-        }));
-        setAuthors(normalizedAuthors);
-      } catch (error) {
-        setIsError(true);
-        toast.warning('No authors found', {
-          style: {
-            backgroundColor: 'rgba(209, 224, 216, 1)',
-            color: '#333',
-          },
-        });
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getAuthors();
+    loadAuthors(1);
   }, []);
-
-  const loadAuthors = async (page, perPage) => {
-    const res = await fetchAuthors(page, perPage );
-    return res.data;
-  };
-
-  const handleAppendAuthors = (newAuthors) => {
-    setAuthors(prev => [...prev, ...newAuthors]);
-  };
 
   const handleAuthorClick = (authorId) => {
     navigate(`/authors/${authorId}`);
   };
-
   return (
     <>
     <Container>
       <SectionTitle  title = {'Authors'}/>
       {isLoading && <Loader/>}
       { authors &&  
-            <>
-            <AuthorsList authors={authors} onAuthCardClick={handleAuthorClick} />
-            <LoadMore loadData={loadAuthors} onDataLoaded={handleAppendAuthors} perPage={20} />
-            </>    
-      }
-      
+        <>
+          <AuthorsList authors={authors} onAuthCardClick={handleAuthorClick} />
+          <LoadMore 
+            loadData={loadAuthors}
+            onDataLoaded={() => {}}
+            perPage={perPage}
+          />
+        </>
+      }      
     </Container>
     </>
   )
