e1e9870dda1fb7cfaf9b22ec3f913aebb6a588ad
Mykola <ipadikys@gmail.com>
2025-08-04 21:57:14 +0200

    added edit feature

--- src/Pages/CreateArticlePage/CreateArticlePage.jsx
+++ src/Pages/CreateArticlePage/CreateArticlePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import Container from '../../components/container/Container';
 import AddArticleForm from '../../components/AddArticleForm/AddArticleForm';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
+
 const CreateArticlePage = () => {
+  const location = useLocation();
+  const isEditing = location.state?.title && location.state?.text;
+  const editData = location.state || null;
+
   return (
     <Container>
-      <SectionTitle title={'Create Article'} />
-      <AddArticleForm />
+      <SectionTitle title={isEditing ? 'Edit Article' : 'Create Article'} />
+      <AddArticleForm editData={editData} isEditing={isEditing} />
     </Container>
   );
 }
